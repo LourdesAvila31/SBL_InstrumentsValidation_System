a6ea4fc452e75b1f13ec2817a137d6e07cae9e69
Lourdes Marién Avila López <lourdes.avila@uabc.edu.mx>
2025-09-24 21:49:56 -0700

    Add batch script to start internal system server

---
+++ sistema-interno/start-sistema-interno.bat
@@ -0,0 +1,7 @@
+@echo off
+echo ========================================
+echo  Sistema Interno ISO 17025
+echo  Iniciando en http://localhost:8000
+echo ========================================
+cd /d "C:\xampp\htdocs\SBL_SISTEMA_INTERNO\sistema-interno"
+npm run serve
